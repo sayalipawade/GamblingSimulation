9d4db582592a0166f5b8dfc32fdaa9852b0830e3
sayalipawade <sayalipawade05@gmail.com>
2020-05-14 18:29:00 +0530

    Added:Generating random no to check win or loose

--- Gambler.js
+++ Gambler.js
@@ -3,3 +3,25 @@ console.log("Welcome to gambler simulation");
 //variables
 const STAKE_FOR_EVERY_DAY=100
 const BET=1
+
+//variables
+cash=STAKE_FOR_EVERY_DAY
+
+//Generating random no
+var randomValue= Math.random()
+
+//Function to check win or loss 
+function dailyGambling()
+{
+    if (randomValue<0.5) 
+    {
+		console.log("Gambler win")
+        cash=cash+BET
+    }
+    else 
+    {
+		console.log("Gambler Lose")
+        cash=cash-BET
+    }
+}
+dailyGambling()
