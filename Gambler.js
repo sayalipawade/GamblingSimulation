f8b2bb507159f0aa4d2f3a8cd81b8abbed28348b
sayalipawade <sayalipawade05@gmail.com>
2020-05-14 18:49:19 +0530

    Added:For Win or loss 50% would resign for the day

--- Gambler.js
+++ Gambler.js
@@ -1,27 +1,29 @@
 console.log("Welcome to gambler simulation");
 
-//variables
+//constants
 const STAKE_FOR_EVERY_DAY=100
 const BET=1
+const UPPER_LIMIT=STAKE_FOR_EVERY_DAY+(STAKE_FOR_EVERY_DAY * 50 /100)
+const LOWER_LIMIT=STAKE_FOR_EVERY_DAY-(STAKE_FOR_EVERY_DAY* 50 /100)
 
 //variables
 cash=STAKE_FOR_EVERY_DAY
 
-//Generating random no
-var randomValue= Math.random()
-
 //Function to check win or loss 
 function dailyGambling()
 {
-    if (randomValue<0.5) 
+    while(cash>LOWER_LIMIT && cash<UPPER_LIMIT)
     {
-		console.log("Gambler win")
-        cash=cash+BET
-    }
-    else 
-    {
-		console.log("Gambler Lose")
-        cash=cash-BET
+        //Generating random no
+        var randomValue= Math.random()   
+        if (randomValue<0.5) 
+        {
+            cash=cash+BET
+        }
+        else 
+        {
+            cash=cash-BET
+        }
     }
 }
 dailyGambling()
