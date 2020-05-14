9d8d402157d9e0755d7aba0d96c25147a183fca7
sayalipawade <sayalipawade05@gmail.com>
2020-05-14 19:24:33 +0530

    Added:Calculate total amount of gambler after 20 days

--- Gambler.js
+++ Gambler.js
@@ -1,17 +1,23 @@
-console.log("Welcome to gambler simulation");
+console.log("Welcome to gambler simulation")
 
 //constants
 const STAKE_FOR_EVERY_DAY=100
 const BET=1
 const UPPER_LIMIT=STAKE_FOR_EVERY_DAY+(STAKE_FOR_EVERY_DAY * 50 /100)
 const LOWER_LIMIT=STAKE_FOR_EVERY_DAY-(STAKE_FOR_EVERY_DAY* 50 /100)
+const TOTAL_DAYS=20
 
 //variables
-cash=STAKE_FOR_EVERY_DAY
+var cash=0
+var winningCash=0
+var totalAmount=0
+var dictionary={}
 
-//Function to check win or loss 
+//Function to calculate daily gambling
 function dailyGambling()
 {
+    winningCash=0
+    cash=STAKE_FOR_EVERY_DAY
     while(cash>LOWER_LIMIT && cash<UPPER_LIMIT)
     {
         //Generating random no
@@ -25,5 +31,27 @@ function dailyGambling()
             cash=cash-BET
         }
     }
+    winningCash=cash-STAKE_FOR_EVERY_DAY
+    return winningCash
 }
-dailyGambling()
+
+//Function to calculate monthly gambling
+function monthlyGambling()
+{
+    for(day=1;day<=TOTAL_DAYS;day++)
+    {
+        var cash=dailyGambling()
+        dictionary[day]=cash
+        totalAmount=totalAmount+dictionary[day];
+        console.log(dictionary[day])
+    }
+    if(totalAmount>0)
+    {
+        console.log("Total amount of 20 days:"+totalAmount)
+    }
+    else
+    {
+        console.log("Total amount of 20 days:"+totalAmount)
+    }
+}
+monthlyGambling()
