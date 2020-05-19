9014cbcdd4e65c7745b1a9cb74db76ae6f6b52b0
sayalipawade <sayalipawade05@gmail.com>
2020-05-19 11:41:36 +0530

    Refactored:Export module and use oop concepts

--- Gambler.js
+++ Gambler.js
@@ -1,5 +1,3 @@
-console.log("Welcome to gambler simulation")
-
 /**
  * constants
  */
@@ -18,94 +16,96 @@ var totalAmount=0
 var sumOfAmount={}
 var lucky_day=0
 
-/**
- * Function to calculate daily gambling
- */
-function dailyGambling()
+class Gambler
 {
-    winningCash=0
-    cash=STAKE_FOR_EVERY_DAY
-    while(cash>LOWER_LIMIT && cash<UPPER_LIMIT)
+    /**
+     * Function to calculate daily gambling
+     */
+    dailyGambling()
     {
-        //Generating random no
-        var randomValue= Math.random()   
-        if (randomValue<0.5) 
+        winningCash=0
+        cash=STAKE_FOR_EVERY_DAY
+        while(cash>LOWER_LIMIT && cash<UPPER_LIMIT)
         {
-            cash=cash+BET
+            //Generating random value
+            var randomValue= Math.random()   
+            if (randomValue<0.5) 
+            {
+                cash=cash+BET
+            }
+            else 
+            {
+                cash=cash-BET
+            }
         }
-        else 
-        {
-            cash=cash-BET
-        }
+        winningCash=cash-STAKE_FOR_EVERY_DAY
+        return winningCash
     }
-    winningCash=cash-STAKE_FOR_EVERY_DAY
-    return winningCash
-}
 
-/**
- * Function to calculate monthly gambling
- */
-function monthlyGambling()
-{
-    for(day=1;day<=TOTAL_DAYS;day++)
-    {
-        var cash=dailyGambling()
-        sumOfAmount[day]=cash
-        totalAmount=totalAmount+sumOfAmount[day];
-        console.log("Amount of day:"+sumOfAmount[day])
-    }
-    if(totalAmount>0)
-    {
-        console.log("Total amount of 20 days:"+totalAmount)
-    }
-    else
-    {
-        console.log("Total amount of 20 days:"+totalAmount)
-    }
-    sort()
-}
-
-/**
- * function for sorting the amount
- */
-function sort()
- {
-    //Creating array of amount
-    var amount = Object.keys(sumOfAmount).map(function(key)
-     {
-        return [key,sumOfAmount[key]];
-     });
-  
-    //Sort the array based on the second element
-    amount.sort(function(first, second)
-     {
-        return second[1] - first[1];
-     });
-  
-    //Luckiest Day
-    console.log("Luckiest Day : ")
-    console.log(amount.slice(0,1));
-
-    //Unluckiest Day
-    console.log("Unluckiest Day : ")
-    console.log(amount.slice(amount.length-2, amount.length-1));
-}
-
-/**
- * function to checking gambler playing for next month or not
- */
-function playNextMonth() 
-{
+    /**
+     * Function to calculate monthly gambling
+     */
     monthlyGambling()
-    if(totalAmount>0)
     {
-        console.log("Gambler playing for next month");
+        for(let day=1;day<=TOTAL_DAYS;day++)
+        {
+            var cash=this.dailyGambling()
+            sumOfAmount[day]=cash
+            totalAmount=totalAmount+sumOfAmount[day];
+            //console.log("Amount of day:"+sumOfAmount[day])
+        }
+        if(totalAmount>0)
+        {
+            console.log("Total amount of 20 days:"+totalAmount)
+        }
+        else
+        {
+            console.log("Total amount of 20 days:"+totalAmount)
+        }
+        this.sort()
     }
-    else
+
+    /**
+     * function for sorting the amount
+     */
+    sort()
     {
-        console.log("Stop gambling");
+        //Creating array of amount
+        var amount = Object.keys(sumOfAmount).map(function(key)
+        {
+            return [key,sumOfAmount[key]];
+        });
+  
+        //Sort the array based on the second element
+        amount.sort(function(first, second)
+        {
+            return second[1]-first[1];
+        });
+  
+        //Luckiest Day
+        console.log("Luckiest Day : ")
+        console.log(amount.slice(0,1));
+
+        //Unluckiest Day
+        console.log("Unluckiest Day : ")
+        console.log(amount.slice(amount.length-2, amount.length-1));
+    }
+
+    /**
+     * function to checking gambler playing for next month or not
+     */
+    playNextMonth() 
+    {
+        this.monthlyGambling()
+        let randomPlay=Math.floor(Math.random()*10)%2
+        if(randomPlay==1)
+        {
+            console.log("Gambler playing for next month");
+        }
+        else
+        {
+            console.log("Stop gambling");
+        }
     }
 }
-playNextMonth()
- 
-   
+module.exports=Gambler
---
+++ main.js
@@ -0,0 +1,11 @@
+//Welcome message
+ console.log("Welcome to gambler simulation");
+
+//import module:Gambler
+ const Gambler=require('./Gambler')
+
+//Object of class
+ const gambler=new Gambler();
+
+//starting of game
+ gambler.playNextMonth()
